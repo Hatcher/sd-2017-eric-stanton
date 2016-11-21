478b852fbd5088c2d07644fa1c5397d659219f95
BLACK_UNICORN\Eric_Stanton <stanton.eric@gmail.com>
2016-11-20 20:58:52 -0500

    Added ability to provide answer choices to user.  Includes 1 correct answer and 3 incorrect answers.

--- modules/math/app/beans/math/MathBean.java
+++ modules/math/app/beans/math/MathBean.java
@@ -1,7 +1,12 @@
 package beans.math;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
+
+import net.sourceforge.jeval.EvaluationException;
+import net.sourceforge.jeval.Evaluator;
+import services.equation.Operators;
 
 public class MathBean {
 
@@ -10,20 +15,19 @@ public class MathBean {
 	private List<String> operators = new ArrayList<String>();
 	private Set<String> types = new HashSet<String>();
 
-	
-	
-	public MathBean(){
+	public MathBean() {
 		this.types.add("Arithmetic");
 		this.types.add("WordProblem");
 	}
-	
-	public String getChosenType(){
+
+	public String getChosenType() {
 		return chosenType;
 	}
-	public void setChosenType(String chosenType){
-		this.chosenType=chosenType;
+
+	public void setChosenType(String chosenType) {
+		this.chosenType = chosenType;
 	}
-	
+
 	public List<BigInteger> getIntegers() {
 		return integers;
 	}
@@ -36,14 +40,64 @@ public class MathBean {
 		return types;
 	}
 
+	public String getAnswer() {
+		Evaluator eval = new Evaluator();
+		try {
+			return eval.evaluate(this.toString());
+		} catch (EvaluationException e) {
+			return "";
+		}
+	}
+
+	public Set<String> getAnswerChoices(String answer) {
+		Set<String> answers = new HashSet<String>();
+		answers.add(answer);
+		Evaluator eval = new Evaluator();
+		Random random = new Random();
+		
+		while (answers.size() < 3) {
+			List<BigInteger> wrongIntegers = cloneIntegers();
+			List<String> wrongOperators = cloneOperators();
+			
+			try {
+				if (random.nextInt(2) >0){
+					wrongIntegers.set(random.nextInt(wrongIntegers.size()), BigInteger.valueOf(random.nextInt(9)+1));
+				}
+				else{
+					wrongOperators.set(random.nextInt(wrongOperators.size()), Operators.ALL_OPERATORS.get(random.nextInt(Operators.ALL_OPERATORS.size())));
+				}
+				answers.add(eval.evaluate(this.toString()));
+			} catch (EvaluationException e) {
+				return new HashSet<String>();
+			}
+		}
+		return answers;
+
+	}
+
+	private List<BigInteger> cloneIntegers(){
+		List<BigInteger> wrongIntegers = new ArrayList<BigInteger>();
+		for (BigInteger tmp : getIntegers()){
+			wrongIntegers.add(BigInteger.valueOf(tmp.intValue()));
+		}
+		return wrongIntegers;
+	}
+
+	private List<String> cloneOperators(){
+		List<String> wrongOperators = new ArrayList<String>();
+		for (String tmp : getOperators()){
+			wrongOperators.add(tmp);
+		}
+		return wrongOperators;
+	}
+	
 	public String toString() {
 		String rValue = "";
 		for (int i = 0; i < getOperators().size(); i++) {
-			if (rValue.equals("")){
-				rValue += getIntegers().get(i)+" "+getOperators().get(i)+" "+getIntegers().get(i+1);
-			}
-			else{
-				rValue += getOperators().get(i)+" "+getIntegers().get(i+1);
+			if (rValue.equals("")) {
+				rValue += getIntegers().get(i) + " " + getOperators().get(i) + " " + getIntegers().get(i + 1);
+			} else {
+				rValue += getOperators().get(i) + " " + getIntegers().get(i + 1);
 			}
 		}
 		return rValue;
